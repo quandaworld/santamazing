c039a959ac7131a51d9769eca61f764a3fa67f2b
quandaworld <nhiquannguyen@gmail.com>
2025-02-24 07:13:41 -0800

    Refactor code

--- js/app.js
+++ js/app.js
@@ -2,17 +2,13 @@
 const singleWallSize = 20;
 const mazeHeight = 300;
 const mazeWidth = 300;
-
+const totalGridRows = mazeHeight / singleWallSize; // 15
+const totalGridCols = mazeWidth / singleWallSize; // 15
 const gridArr = [];
 const wallLeftCoords = [];
 const wallRightCoords = [];
 const wallTopCoords = [];
 const wallBotCoords = [];
-
-
-/*---------------------------- Variables (state) ----------------------------*/
-let totalGridRows = mazeHeight / singleWallSize; // 15
-let totalGridCols = mazeWidth / singleWallSize; // 15
 
 /*------------------------ Cached Element References ------------------------*/
 const maze = document.getElementById('maze');
@@ -33,55 +29,57 @@ window.onload = () => {
 	}
 };
 
-// Populate grid arr structure with rows and columns
+// Populate grid array structure with rows and columns
 function initializeGrid() {
 	for (let i = 0; i < totalGridRows; i++) {
 		const singleRowArr = [];
-	
+
 		for (let j = 0; j < totalGridCols; j++) {
 			singleRowArr.push({ u: 0, d: 0, l: 0, r: 0, v: 0 }); // up, down, left, right, visited
 		}
-	
+
 		gridArr.push(singleRowArr);
 	}
 }
 
-// Create vertical walls with random entry and exit points
-function createVerticalWalls() {
+// Create maze's vertical boundaries with random entry and exit gaps
+function createVerticalBoundaries() {
 	const topHeight = Math.floor(Math.random() * totalGridRows) * singleWallSize;
 	const botHeight = mazeHeight - singleWallSize - topHeight;
+	const rightWall_x = mazeWidth + singleWallSize;
 
-	const wallArr = [
-			{ top: singleWallSize, left: singleWallSize, height: topHeight },
-			{ top: topHeight + singleWallSize * 2, left: singleWallSize, height: botHeight },
-			{ top: singleWallSize, left: mazeWidth + singleWallSize, height: botHeight },
-			{ top: botHeight + singleWallSize * 2, left: mazeWidth + singleWallSize, height: topHeight }
+	const walls = [
+		{ t: singleWallSize, l: singleWallSize, h: topHeight },
+		{ t: topHeight + 2 * singleWallSize, l: singleWallSize, h: botHeight },
+		{ t: singleWallSize, l: rightWall_x, h: botHeight },
+		{ t: botHeight + 2 * singleWallSize, l: rightWall_x, h: topHeight }
 	];
 
 	// Store wall coordinates
-	wallLeftCoords.push(0, mazeWidth + 2 * singleWallSize, 0, 0, mazeWidth + singleWallSize, mazeWidth + singleWallSize);
-	wallRightCoords.push(0 + wallWidth, mazeWidth + 2 * singleWallSize + wallWidth, singleWallSize, singleWallSize, mazeWidth + 2 * singleWallSize, mazeWidth + 2 * singleWallSize);
-	wallTopCoords.push(topHeight + singleWallSize, botHeight + singleWallSize, topHeight + singleWallSize, topHeight + 2 * singleWallSize, botHeight + singleWallSize, botHeight + 2 * singleWallSize);
-	wallBotCoords.push(topHeight + 2 * singleWallSize, botHeight + 2 * singleWallSize, topHeight + singleWallSize + wallWidth, topHeight + 2 * singleWallSize + wallWidth, botHeight + singleWallSize + wallWidth, botHeight + 2 * singleWallSize + wallWidth);
+	wallLeftCoords.push(0, rightWall_x, 0, 0, rightWall_x, rightWall_x);
+	wallRightCoords.push(wallWidth, rightWall_x + wallWidth, singleWallSize, singleWallSize, rightWall_x, rightWall_x);
+	wallTopCoords.push(topHeight + singleWallSize, botHeight + singleWallSize, topHeight + singleWallSize,
+		topHeight + 2 * singleWallSize, botHeight + singleWallSize, botHeight + 2 * singleWallSize);
+	wallBotCoords.push(...wallTopCoords.map(coord => coord + wallWidth));
 
-	// Set player & exit positions
+	// Set Santa entry & exit position
 	Object.assign(santa.style, { top: `${topHeight + singleWallSize}px`, left: '0px' });
-	Object.assign(exit.style, { top: `${botHeight + singleWallSize}px`, left: `${mazeWidth + singleWallSize}px` });
+	Object.assign(exit.style, { top: `${botHeight + singleWallSize}px`, left: `${rightWall_x}px` });
 
 	// Create walls
-	wallArr.forEach(({ top, left, height }) => {
-			const wall = document.createElement('div');
-			Object.assign(wall.style, { top: `${top}px`, left: `${left}px`, height: `${height}px`, width: `${wallWidth}px` });
-			wall.classList.add('wall');
-			maze.appendChild(wall);
+	walls.forEach(({ t, l, h }) => {
+		const wall = document.createElement('div');
+		Object.assign(wall.style, { top: `${t}px`, left: `${l}px`, height: `${h}px`, width: `${wallWidth}px` });
+		wall.classList.add('wall');
+		maze.appendChild(wall);
 	});
 }
 
 // Helper method for generateRandomMaze()
 function shuffleArr(arr) {
 	for (let i = arr.length - 1; i >= 0; i--) {
-			const j = Math.floor(Math.random() * (i + 1));
-			[arr[i], arr[j]] = [arr[j], arr[i]];
+		const j = Math.floor(Math.random() * (i + 1));
+		[arr[i], arr[j]] = [arr[j], arr[i]];
 	}
 
 	return arr;
@@ -90,7 +88,7 @@ function shuffleArr(arr) {
 // Populate a maze using Recursive Backtracking
 function generateRandomMaze(currentX, currentY) {
 	const directions = shuffleArr(['u', 'd', 'l', 'r']); // up, down, left, right
-	const dirsCoords = { // directions' coordinates
+	const dirsCoords = { // directions' coordinates x, y, opposite
 		u: { x: 0, y: -1, o: 'd' },
 		d: { x: 0, y: 1, o: 'u' },
 		l: { x: -1, y: 0, o: 'r' },
@@ -98,53 +96,62 @@ function generateRandomMaze(currentX, currentY) {
 	};
 
 	for (let i = 0; i < directions.length; i++) {
-		let nx = currentX + dirsCoords[directions[i]].x;
-		let ny = currentY + dirsCoords[directions[i]].y;
+		const newX = currentX + dirsCoords[directions[i]].x;
+		const newY = currentY + dirsCoords[directions[i]].y;
+
 		gridArr[currentY][currentX].v = 1;
 
-		if (nx >= 0 && nx < totalGridCols && ny >= 0 && ny < totalGridRows && gridArr[ny][nx].v === 0) {
+		if (newX >= 0 && newX < totalGridCols && newY >= 0 && newY < totalGridRows && gridArr[newY][newX].v === 0) {
 			gridArr[currentY][currentX][directions[i]] = 1;
-			gridArr[ny][nx][dirsCoords[directions[i]].o] = 1;
-			generateRandomMaze(nx, ny);
+			gridArr[newY][newX][dirsCoords[directions[i]].o] = 1;
+			generateRandomMaze(newX, newY);
 		}
 	}
 }
 
-// helper method for drawMaze
-function drawLines(x, y, l, r, u, d) {
-	let top = (y + 1) * singleWallSize;
-	let left = (x + 1) * singleWallSize;
+// helper method for displayMaze
+function createMazeWalls(x, y, u, d, l, r) {
+	const top = (y + 1) * singleWallSize;
+	const left = (x + 1) * singleWallSize;
+
+	const walls = [];
+
 	if (l === 0 && x > 0) {
-		let el = document.createElement('div');
-		el.style.left = left + 'px';
-		el.style.height = singleWallSize + 'px';
-		el.style.top = top + 'px';
-		el.setAttribute('class', 'wall');
-		el.style.width = wallWidth + 'px';
-		maze.appendChild(el);
+		walls.push({
+			top,
+			left,
+			width: wallWidth,
+			height: singleWallSize,
+		});
 	}
 
 	if (d === 0 && y < totalGridRows - 1) {
-		let el = document.createElement('div');
-		el.style.left = left + 'px';
-		el.style.height = wallWidth + 'px';
-		el.style.top = top + singleWallSize + 'px';
-		el.setAttribute('class', 'wall');
-		el.style.width = singleWallSize + wallWidth + 'px';
-		maze.appendChild(el);
+		walls.push({
+			top: top + singleWallSize,
+			left,
+			width: singleWallSize + wallWidth,
+			height: wallWidth,
+		});
 	}
+
+	walls.forEach(({ top, left, width, height }) => {
+		const el = document.createElement('div');
+		Object.assign(el.style, { top: `${top}px`, left: `${left}px`, width: `${width}px`, height: `${height}px` });
+		el.classList.add('wall');
+		maze.appendChild(el);
+	});
 }
 
 // Draw maze
-function drawMaze() {
+function displayMaze() {
 	for (let x = 0; x < totalGridCols; x++) {
 		for (let y = 0; y < totalGridRows; y++) {
-			let l = gridArr[y][x].l;
-			let r = gridArr[y][x].r;
-			let u = gridArr[y][x].u;
-			let d = gridArr[y][x].d;
+			const l = gridArr[y][x].l;
+			const r = gridArr[y][x].r;
+			const u = gridArr[y][x].u;
+			const d = gridArr[y][x].d;
 
-			drawLines(x, y, l, r, u, d);
+			createMazeWalls(x, y, u, d, l, r);
 		}
 	}
 }
@@ -156,7 +163,7 @@ function checkXWall(direction) {
 
 // check if one can move vertically
 function checkYWall(direction) {
-	
+
 }
 
 function up() {
@@ -203,9 +210,9 @@ function handleKeyboardInput(e) {
 }
 
 initializeGrid();
-createVerticalWalls();
+createVerticalBoundaries();
 generateRandomMaze(0, 0);
-drawMaze();
+displayMaze();
 
 /*----------------------------- Event Listeners -----------------------------*/
 document.addEventListener('keydown', handleKeyboardInput);
